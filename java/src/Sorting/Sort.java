e5080b83b9fe5c77f0771ea4e1a20cbfc7a90897
GerardoDRM <gerardo.delarosama@udlap.mx>
2017-07-26 00:48:44 -0500

    Adding Quick Sort and Hashtable ransom

--- java/src/Sorting/Sort.java
+++ java/src/Sorting/Sort.java
@@ -1,5 +1,8 @@
 package Sorting;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 /**
  * Created by gerardo on 25/07/17.
  */
@@ -20,4 +23,63 @@ public class Sort {
 
         System.out.println(swapCounter);
     }
+
+    // Ranson Note
+    public static boolean subMessage(String[] n, String []n2) {
+        HashMap<String, Integer> values = new HashMap<>();
+        for(String s1:n) {
+            if(values.containsKey(s1))
+                values.put(s1, values.get(s1) + 1);
+            else
+                values.put(s1, 1);
+
+        }
+
+
+        for (String s2:n2) {
+            if(!values.containsKey(s2) || values.get(s2)-1 <= 0) {
+                return false;
+            } else {
+                values.put(s2, values.get(s2) -1);
+            }
+        }
+        return true;
+    }
+
+
+    // Quick Sort
+    public static void  quicksort(int[] array) {
+        quicksort(array, 0, array.length);
+    }
+
+    private static void quicksort(int[] array, int left, int right) {
+        if(left<=right) return;
+        // Get pivot
+        int pivot = (left+right) / 2;
+        int index = partition(array, left, right, pivot);
+        quicksort(array, left, index-1);
+        quicksort(array, index, right);
+    }
+
+    private static int partition(int[] array, int left, int right, int p) {
+        while(left<=right) {
+            while(array[left] < p) {
+                left++;
+            }
+            while(array[right] < p) {
+                left--;
+            }
+
+            if(left <= right) {
+                int temp = array[left];
+                array[temp] = array[right];
+                array[right] = temp;
+                right--;
+                left++;
+            }
+        }
+        return left;
+    }
+
+
 }
